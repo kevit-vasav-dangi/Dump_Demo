d3418e7d3d8ca6ece07d4da71652ccf846a5a08c
vasav <vasav.dangi@kevit.io>
2022-04-06 17:44:58 +0530

    new finding

--- rename.js
+++ rename.js
@@ -1,17 +1,24 @@
 const fs = require('fs');
 let a = [];
+let c;
 getCurrentFilenames = async () => {
     console.log("Current filenames:");
     fs.readdirSync(__dirname).forEach(file => {
         file;
-        a.push(file)
+        a.push('/'+file+'/')
         //console.log(file);
     });
 }
 getCurrentFilenames()
 //let b = a.toString().split("yearly_backup-")
 //console.log(b[1]);
-console.log(a[10]);
+a=a.toString().split(',')
+//console.log(a[6]);
+c = a[7].split('backup-'&&'-00');
+console.log(c);
+// console.log(a);
+//c = (''+a+"").split(',')
+//console.log(c);
 // for (let i = 0; a.length; i++) {
 
 //     var dateType = /(\d{4})([\/-])(\d{1,2})\2(\d{1,2})/;
